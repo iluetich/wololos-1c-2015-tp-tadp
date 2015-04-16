ec5b1af175894467d08f04c580be7c7636ba4ba6
iluetich <ivan.luetich93@gmail.com>
2015-04-16 02:17:11 -0300

    Agrego más casos de prueba para excepciones

--- spec/partial_block_spec.rb
+++ spec/partial_block_spec.rb
@@ -1,8 +1,9 @@
 require 'rspec'
 require_relative '../src/partial_block'
+require_relative '../src/exceptions/exceptions'
 
 
-describe "PartialBlock tests" do
+describe "Calling PartialBlocks with correct parameters" do
 
   it "Devuelve un Hello mas parámetro String" do
     helloblock = PartialBlock.new([String]) do |who|
@@ -31,6 +32,30 @@ describe "PartialBlock tests" do
       n + m
     end
     expect(sumador.call(2,3)).to eq(5)
+  end
+
+end
+
+describe "Calling PartialBlocks with incorrect parameters" do
+  it "Si llamo a un bloque con menos parámetros tira ArgumentsException" do
+    bloque_a_romper = PartialBlock.new([Integer, String]) do |numero, palabra|
+      numero * 2
+    end
+      expect { bloque_a_romper.call(10) }.to raise_error(Exception::ArgumentsException)
+  end
+
+  it "Si llamo a un bloque con más parámetros tira ArgumentsException" do
+    bloque_a_romper = PartialBlock.new([String, Object]) do |p, q|
+      p
+    end
+    expect { bloque_a_romper.call("Hola", Object.new, 1000) }.to raise_error(Exception::ArgumentsException)
+  end
+
+  it "Si llamo a un bloque con parámetros de tipos erróneos tira ArgumentsTypeException" do
+    bloque_a_estallar = PartialBlock.new([Integer, Integer]) do |a, b|
+      a + b * a
+    end
+    expect { bloque_a_estallar.call(10, "super saraza") }.to raise_error(Exception::ArgumentsTypeException)
   end
 
 end
