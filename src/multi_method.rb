0438379043d96d7eb76a652986f6776a2f837507
iluetich <ivan.luetich93@gmail.com>
2015-04-19 23:53:52 -0300

    Identing and fixing typos

--- src/multi_method.rb
+++ src/multi_method.rb
@@ -4,10 +4,6 @@ require_relative '../src/partial_block'
 
 module MultiMethods
 
-  attr_accessor :lista_de_multimetodos
-
-  @@lista_de_multimetodos = []
-
   def partial_def(nombre_metodo, lista_de_tipos, &bloque)
 
     bloque_parcial = PartialBlock.new(lista_de_tipos, &bloque)
@@ -15,14 +11,18 @@ module MultiMethods
     self.add_sobrecarga(una_sobrecarga)
     self.send(:define_method, nombre_metodo) { |*argumentos|
       my_name = nombre_metodo
-      sobrecargas = self.class.obtener_sobrecargas_validas(@@lista_de_multimetodos, my_name, *argumentos)
+      sobrecargas = self.class.obtener_sobrecargas_validas(self.class.multimetodos, my_name, *argumentos)
       self.class.elegir_mas_cercano(sobrecargas, *argumentos).call(*argumentos)
     }
 
   end
 
+  def multimetodos
+    @lista_de_multimetodos = @lista_de_multimetodos || []
+  end
+
   def add_sobrecarga(una_sobrecarga)
-    @@lista_de_multimetodos << una_sobrecarga
+    self.multimetodos << una_sobrecarga
   end
 
   def obtener_sobrecargas_validas(sobrecargas, nombre_sobrecarga, *argumentos)
@@ -34,48 +34,3 @@ module MultiMethods
   end
 
 end
-
-
-
-=begin
-
-    ##### EJEMPLOS DE USO #####
-
-
-class A
-  extend MultiMethods
-
-  partial_def :concat, [String,String] do |s1,s2|
-    s1 + s2
-  end
-
-  partial_def :concat, [String,Integer] do |s1,s2|
-    s1 * s2
-  end
-
-  partial_def :concat, [Object,Object] do
-    "dos objetos felices"
-  end
-
-  partial_def :concat, [String] do
-    "un solo string!"
-  end
-
-  partial_def :concat, [Fixnum] do
-    "soy un fixnum"
-  end
-
-  partial_def :saluda, [String, String, String] do |s1, s2, s3|
-   s1 + ' ' + s2 + ' ' + s3
-  end
-
-end
-
-puts A.new.concat('hello',' world!')
-puts A.new.concat('hola ',3)
-a = A.new
-puts a.concat('hola ')
-puts a.concat(3)
-puts a.saluda('hola,', 'todo', 'bien?')
-
-=end
