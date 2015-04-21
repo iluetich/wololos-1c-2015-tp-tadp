26dfb9e1854a2271176b97f008b2e09083745d84
iluetich <ivan.luetich93@gmail.com>
2015-04-21 17:21:56 -0300

    Ahora la ejecución es dentro del scope de la instancia

--- src/multi_method.rb
+++ src/multi_method.rb
@@ -7,7 +7,8 @@ module MultiMethods
   def crear_multimetodo(nombre_multimetodo)
     self.send(:define_method, nombre_multimetodo) { |*argumentos|
       sobrecargas = self.class.obtener_sobrecargas_validas(self.class.multimetodos, nombre_multimetodo, *argumentos)
-      self.class.elegir_mas_cercano(sobrecargas, *argumentos).call(*argumentos)
+      comportamiento = self.class.obtener_partial_block(sobrecargas, *argumentos).bloque
+      self.instance_exec *argumentos, &comportamiento
     }
   end
 
@@ -32,8 +33,8 @@ module MultiMethods
     sobrecargas.select { |s| s.nombre == nombre_sobrecarga && s.matches(*argumentos) }
   end
 
-  def elegir_mas_cercano(sobrecargas, *argumentos)
-    sobrecargas.min_by { |m| m.distancia_a_parametros(*argumentos) }
+  def obtener_partial_block(sobrecargas, *argumentos)
+    sobrecargas.min_by { |m| m.distancia_a_parametros(*argumentos) }.bloque
   end
 
 end
