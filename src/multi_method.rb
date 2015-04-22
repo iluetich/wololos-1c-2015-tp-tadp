99eb03097e61b6e6f853863e154c7cf43983c1c7
iluetich <ivan.luetich93@gmail.com>
2015-04-21 21:21:20 -0300

    Ordenando más código. Ahora se sigue con method LookUp cuando no se encuentra el multimétodo

--- src/multi_method.rb
+++ src/multi_method.rb
@@ -6,22 +6,27 @@ module MultiMethods
 
   def crear_multimetodo(nombre_multimetodo)
     self.send(:define_method, nombre_multimetodo) { |*argumentos|
-      sobrecargas = self.class.obtener_sobrecargas_validas(self.class.multimetodos, nombre_multimetodo, *argumentos)
-      comportamiento = self.class.obtener_partial_block(sobrecargas, *argumentos).bloque
-      self.instance_exec *argumentos, &comportamiento
+      begin
+        comportamiento = self.class.obtener_partial_block_cercano(nombre_multimetodo, *argumentos).bloque
+        instance_exec(*argumentos, &comportamiento)
+      rescue NoMethodError
+        #Seguir con el method LookUp
+        super(*argumentos)
+      end
     }
   end
 
   def partial_def(nombre_multimetodo, lista_de_tipos, &bloque)
-    bloque_parcial = PartialBlock.new(lista_de_tipos, &bloque)
-    una_sobrecarga = Sobrecarga.new(nombre_multimetodo, bloque_parcial)
-    self.agregar_sobrecargar(una_sobrecarga, nombre_multimetodo, bloque_parcial)
-    #Con el unless nos evitamos pisar las definiciones una y otra vez de una sobrecarga.
+    self.sobre_cargar(Sobrecarga.new(nombre_multimetodo, PartialBlock.new(lista_de_tipos, &bloque)), nombre_multimetodo)
     self.crear_multimetodo(nombre_multimetodo) unless self.respond_to? nombre_multimetodo
   end
 
-  def agregar_sobrecargar(una_sobrecarga, nombre_multimetodo, bloque_parcial)
-    self.multimetodos.delete_if{ |s| s.es_misma_firma(nombre_multimetodo, bloque_parcial) }
+  def sobre_cargar(una_sobrecarga, nombre_multimetodo)
+    self.multimetodos.delete_if{ |s| s.es_misma_firma(nombre_multimetodo, una_sobrecarga.partial_block) }
+    self.agregar_sobre_carga(una_sobrecarga)
+  end
+
+  def agregar_sobre_carga(una_sobrecarga)
     self.multimetodos << una_sobrecarga
   end
 
@@ -29,12 +34,16 @@ module MultiMethods
     @lista_de_multimetodos = @lista_de_multimetodos || []
   end
 
-  def obtener_sobrecargas_validas(sobrecargas, nombre_sobrecarga, *argumentos)
-    sobrecargas.select { |s| s.nombre == nombre_sobrecarga && s.matches(*argumentos) }
+  def select_sobre_cargas(nombre_sobrecarga, *argumentos)
+    self.multimetodos.select { |s| s.nombre == nombre_sobrecarga && s.matches(*argumentos) }
   end
 
-  def obtener_partial_block(sobrecargas, *argumentos)
-    sobrecargas.min_by { |m| m.distancia_a_parametros(*argumentos) }.bloque
+  def obtener_partial_block_cercano(nombre_sobre_carga, *argumentos)
+    sobre_carga = self.select_sobre_cargas(nombre_sobre_carga, *argumentos).min_by { |m| m.distancia_a_parametros(*argumentos) }
+    if sobre_carga.eql? nil
+      raise NoMethodError
+    end
+    sobre_carga.partial_block
   end
 
 end
